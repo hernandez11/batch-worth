76eb856cb417118fdf34e7d3dab51ca008ac2263
Angel Hernandez Diaz <ahernandez11diaz@gmail.com>
2025-11-25 23:18:42 -0700

    removed unused image, added new folder for assets, added new styled wrapper for tab content (WIP)

--- src/layout/tabs/Dashboard.tsx
+++ src/layout/tabs/Dashboard.tsx
@@ -1,10 +1,18 @@
+import { StyledContentWrapper } from '../../theme/StyledContentWrapper'
 import { StyledPrimaryHeader, StyledSubheader } from '../../theme/StyledText'
 
 export default function Dashboard() {
   return (
-    <>
-      <StyledPrimaryHeader data-testid={'DashboardTitle'}>LOLOS PASTRY</StyledPrimaryHeader>
-      <StyledSubheader data-testid={'DashboardSubtitle'}>BATCH CALCULATOR</StyledSubheader>
-    </>
+    <StyledContentWrapper>
+      <div>
+        <StyledPrimaryHeader data-testid={'DashboardTitle'}>LOLOS PASTRY</StyledPrimaryHeader>
+        <StyledSubheader data-testid={'DashboardSubtitle'}>BATCH CALCULATOR</StyledSubheader>
+      </div>
+      <img
+        data-testid={'DashboardBackground'}
+        src="src/assets/dashboardBackground.png"
+        alt="Dashboard background"
+      />
+    </StyledContentWrapper>
   )
 }
