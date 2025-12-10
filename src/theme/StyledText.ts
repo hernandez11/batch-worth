61a3660372417427735a487d926a854555ae035c
Angel Hernandez Diaz <ahernandez11diaz@gmail.com>
2025-12-09 22:55:40 -0500

    uploaded assets and updated styling for responsiveness (WIP)

--- src/theme/StyledText.ts
+++ src/theme/StyledText.ts
@@ -2,16 +2,15 @@ import styled from 'styled-components'
 
 export const StyledPrimaryHeader = styled.header`
   color: #e38a8c;
-  font-size: 8em;
+  font-size: 8vw;
   letter-spacing: -5px;
-  line-height: 100px;
   background-color: white;
   white-space: nowrap;
+  line-height: 100px;
 `
 
 export const StyledSubheader = styled.h3`
   color: #e38a8c;
-  font-size: 2em;
+  font-size: 2.5vw;
   font-weight: 400;
-  line-height: 0px;
 `
