93abfe55974847be9b1d840f4d44ec39071e668b
Angel Hernandez Diaz <ahernandez11diaz@gmail.com>
2025-12-23 11:51:23 -0700

    added responsiveness to styled text

--- src/theme/StyledText.ts
+++ src/theme/StyledText.ts
@@ -2,15 +2,18 @@ import styled from 'styled-components'
 
 export const StyledPrimaryHeader = styled.header`
   color: #e38a8c;
-  font-size: 8vw;
-  letter-spacing: -5px;
-  background-color: white;
+  font-size: clamp(3.5rem, 8vw, 8rem);
+  letter-spacing: -4px;
   white-space: nowrap;
   line-height: 100px;
+
+  @media only screen and (max-width: 768px) {
+    line-height: 75px;
+  }
 `
 
 export const StyledSubheader = styled.h3`
   color: #e38a8c;
-  font-size: 2.5vw;
+  font-size: clamp(1rem, 2.5vw, 2rem);
   font-weight: 400;
 `
